2742902ddec45b50f577c1197a4d710f42a6eef8
Divyansh Singh <divyansh1539@gmail.com>
2025-07-18 00:35:01 +0530

    create login page and security

--- devil/views.py
+++ devil/views.py
@@ -5,6 +5,21 @@ from django.contrib.auth.decorators import login_required
 
 
 def register_page(request):
-    return render(request,"register.html")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
 
+        if password != confirm_password:
+            message.error(request,"❌ Passwords do not match.")
+            return render(request,"register.html")
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        message.success(request,"✅ Account created successfully")
+        return redirect("login")
+    
+    return render("login.html")
 
