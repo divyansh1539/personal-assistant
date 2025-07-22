4e693d3e1c802d436356e08c2efc12c3b97da36d
kapil kumar <kapilkumar232222@gmail.com>
2025-07-22 19:00:35 +0530

    pull

--- devil/static/images/script.js
+++ devil/static/images/script.js
@@ -49,25 +49,3 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-// window.addEventListener("DOMContentLoaded", () => {
-//     const heading = document.getElementById("welcome-heading");
-//     const userName = heading?.dataset?.username || "stranger";
-  
-//     const msg = new SpeechSynthesisUtterance("Welcome, " + userName +  ", You have crossed the line, The devil rides with you now");
-//     msg.pitch = 0.3;       
-//     msg.rate = 0.65;      
-//     msg.volume = 2.0;      
-//     msg.lang = "en-US";  
-//     msg.voice = speechSynthesis.getVoices().find(
-//       voice =>    
-//       voice.name.includes("Microsoft David") ||
-//       voice.name.includes("Google")
-// );
-//       if (speechSynthesis.getVoices().length === 0) {
-//       speechSynthesis.addEventListener("voiceschanged", () => {
-//         speechSynthesis.speak(msg);
-//       });
-//     } else {
-//       speechSynthesis.speak(msg);
-//     }
-//   });
